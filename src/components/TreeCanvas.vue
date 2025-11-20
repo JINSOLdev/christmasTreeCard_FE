d5720fa3df7fe7e162697d69d91b29d0bd307375
jinsol kim <jinsol032360@gmail.com>
2025-11-20 21:06:50 +0900

    fix: 눈송이 스타일 수정

--- src/components/TreeCanvas.vue
+++ src/components/TreeCanvas.vue
@@ -82,31 +82,131 @@ function onClickOrnament(card: Card) {
   user-select: none;
 }
 
-/* 눈 효과 등 기존 스타일 그대로 */
+/* 공통 눈 스타일 */
 .snow {
   position: absolute;
-  inset: 0;
-  background-image: radial-gradient(white 1.5px, transparent 1.5px);
-  opacity: 0.8;
-  animation: snow-fall 12s linear infinite;
+  width: 5px;
+  height: 5px;
+  background: white;
+  border-radius: 50%;
+  opacity: 0.9;
+  filter: drop-shadow(0 0 3px rgba(255, 255, 255, 0.7));
+  animation-name: snow-fall;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
 }
 
+/* 레이어 1 */
+.snow-1 {
+  top: -80px;
+  left: 0;
+  animation-duration: 8s;
+  animation-delay: 0s;
+  box-shadow:
+    10px -60px white,
+    40px -25px white,
+    70px -5px white,
+    100px -45px white,
+    130px -15px white,
+    160px -35px white,
+    190px -10px white,
+    220px -50px white,
+    250px -20px white;
+}
+
+/* 레이어 2 */
 .snow-2 {
-  animation-duration: 16s;
-  opacity: 0.6;
+  top: -90px;
+  left: 15px;
+  animation-duration: 9s;
+  animation-delay: 1s;
+  box-shadow:
+    5px -40px white,
+    35px -10px white,
+    65px -55px white,
+    95px -20px white,
+    125px -70px white,
+    155px -30px white,
+    185px -5px white,
+    215px -45px white,
+    245px -15px white;
 }
 
+/* 레이어 3 */
 .snow-3 {
-  animation-duration: 20s;
-  opacity: 0.5;
+  top: -100px;
+  left: 30px;
+  animation-duration: 10s;
+  animation-delay: 2s;
+  box-shadow:
+    0px -75px white,
+    30px -35px white,
+    60px -5px white,
+    90px -50px white,
+    120px -25px white,
+    150px -65px white,
+    180px -15px white,
+    210px -40px white,
+    240px -10px white;
+}
+
+/* 레이어 4 */
+.snow-4 {
+  top: -85px;
+  left: 5px;
+  animation-duration: 8.5s;
+  animation-delay: 0.5s;
+  box-shadow:
+    20px -55px white,
+    50px -15px white,
+    80px -40px white,
+    110px -5px white,
+    140px -60px white,
+    170px -25px white,
+    200px -70px white,
+    230px -20px white;
+}
+
+/* 레이어 5 */
+.snow-5 {
+  top: -95px;
+  left: 25px;
+  animation-duration: 9.5s;
+  animation-delay: 1.5s;
+  box-shadow:
+    15px -65px white,
+    45px -30px white,
+    75px -10px white,
+    105px -55px white,
+    135px -20px white,
+    165px -75px white,
+    195px -35px white,
+    225px -5px white;
+}
+
+/* 레이어 6 */
+.snow-6 {
+  top: -105px;
+  left: 35px;
+  animation-duration: 11s;
+  animation-delay: 3s;
+  box-shadow:
+    10px -50px white,
+    40px -80px white,
+    70px -25px white,
+    100px -60px white,
+    130px -15px white,
+    160px -45px white,
+    190px -5px white,
+    220px -70px white;
 }
 
 @keyframes snow-fall {
   0% {
-    transform: translateY(-40px);
+    transform: translateY(-60px);
   }
   100% {
-    transform: translateY(40px);
+    transform: translateY(420px);
   }
 }
 </style>
